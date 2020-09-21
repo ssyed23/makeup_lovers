53a61928a6e20f8b58aadcd8e1ee31ed8fe2856d
ssyed23 <33768041+ssyed23@users.noreply.github.com>
2020-09-20 23:48:16 -0400

    Update search_products.py

--- search_products.py
+++ search_products.py
@@ -51,30 +51,44 @@ class search_product:
 #item_name = input("Please enter the brand you are interested in: ")
 
 print("Here are the products by " + item_name + ": ")
-for i in range(0,166):
-  if data[i]['brand'] == item_name:
-    print(data[i]['name'])
-    print("$ " + data[i]["price"])
-    print(data[i]["product_link"])
-    #print(data[i]['description'])
+    self.brand_name.append(item_name)
+    
+    for i in range(0,166):
+     if data[i]['brand'] == item_name:
+       name_product = data[i]['name']
+       price_product = data[i]["price"]
+       product_link = data[i]["product_link"]
+       product_id = data[i]["id"]
 
-shopping_cart=[]
-add_to_cart = input("Would you like to add this to your cart? ")
-if add_to_cart == "yes":
-  shopping_cart.append(data[i]["name"])
-  with open('shopping_Cart.json', 'w') as output_file:
-    new =json.dumps(shopping_cart,default=lambda o: o.__dict__, sort_keys=True, indent= 4)
-    json.dump(new,output_file)
+       print(name_product)
+       print(price_product)
+       print(product_link)
+       print(product_id)
+
+    self.purchase_items()
+       
+       
+  def purchase_items(self):
+
+    data = self.r.json()
+    product_id = input("what is the id of the product you want to buy ? ")
+    how_many = int(input("quantitiety: "))
+
+    for i in range(0,166):
+      if data[i]['name'] == product_id:
+        print(data[i]['id'])
+        print(data[i]['price'])
+        print(data[i]['link'])
+
+        cost_of_item = data[i]['price']
+        print(cost_of_item)
 
 
 
 
 
-#def __init__(self):#,name):
- # base_url = "https://makeup-api.herokuapp.com/api/v1/products.json/product_type=foundation"
-  #name = name.upper()
- # query_param = {"brand": name}
-  #r = requests.get(base_url)#,query_param)
-  #print (r)
 
+
+    # print (r.json())
+# print(r['price'])
 
