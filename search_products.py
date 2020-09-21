ea8d2ed55454265412d95f63226503bcb3f8312d
ssyed23 <33768041+ssyed23@users.noreply.github.com>
2020-09-20 23:47:18 -0400

    Update search_products.py

--- search_products.py
+++ search_products.py
@@ -10,17 +10,46 @@ class search_product:
 
     self.r = requests.get('https://makeup-api.herokuapp.com/api/v1/products.json?product_type=foundation')
 
-
-print(len(data))
-print(data[165])
-print(data[165]['id'])
+    def search_prod(self, item_name):
 
 
-for i in range(0,166):
-  print(data[i]['brand'])
+    if self.r.status_code == 200:
+          print('\nWe were able to find the value of the stock!')
+    elif self.r.status_code == 404:
+          print('Sorry we could not find the stock you were looking for')
+          return(self.search_product)
+
+    
+    data = self.r.json()
+
+    # self.init_response.append(data)
+
+    # print(data)
+    #print(data[165])
+    #print(data[165]['id'])
+    
+
+    # print(len(data))
 
 
-item_name = input("Please enter the brand you are interested in: ")
+    # for i in range(0,166):
+    #   print(data[i]['brand'])
+    
+    #item_name = input("Please enter the brand you are interested in: ")
+    
+
+
+#print(len(data))
+#print(data[165])
+#print(data[165]['id'])
+
+
+#for i in range(0,166):
+  #print(data[i]['brand'])
+
+
+#item_name = input("Please enter the brand you are interested in: ")
+
 print("Here are the products by " + item_name + ": ")
 for i in range(0,166):
   if data[i]['brand'] == item_name:
