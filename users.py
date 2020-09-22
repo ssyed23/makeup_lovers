6daa33d2f8cc7062dc007fd9ae08c92833e9ff49
ssyed23 <33768041+ssyed23@users.noreply.github.com>
2020-09-22 16:32:10 -0500

    Update users.py

--- users.py
+++ users.py
@@ -1,12 +1,9 @@
-
 class Users_C:
   
   def __init__(self, user_name, ID_num):
-    
     self.user_name = user_name
     self.ID_num = ID_num
     self.port={}
   
   def __str__(self):
-    
     return (f"Username: \"{self.user_name}\" | UserID:#{self.ID_num} " + f"{self.initial_investment:,.2f}") 
